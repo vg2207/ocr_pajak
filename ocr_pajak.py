33f05fcc1098e1a9db235b326e7307398589c4e6
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-08-24 18:19:57 +0700

    Update ocr_pajak.py

--- ocr_pajak.py
+++ ocr_pajak.py
@@ -189,12 +189,7 @@ if user_input_folder is not None:
         
                     df_all_data_extracted_combined = pd.concat([df_all_data_extracted_combined, df_all_data_extracted]).reset_index(drop=True)
                     for i in range(len(df_all_data_extracted_combined)):
-                        df_all_data_extracted_combined["PAJAK PENGHASILAN converted"][i] = df_all_data_extracted_combined["PAJAK PENGHASILAN"][i].replace(".","")
-                        df_all_data_extracted_combined["DPP converted"][i] = df_all_data_extracted_combined["DPP"][i].replace(".","")
-                    df_all_data_extracted_combined["PAJAK PENGHASILAN converted"] = df_all_data_extracted_combined["PAJAK PENGHASILAN converted"].astype("float")
-                    df_all_data_extracted_combined["DPP converted"] = df_all_data_extracted_combined["DPP converted"].astype("float")
-                    for i in range(len(df_all_data_extracted_combined)):
-                        df_all_data_extracted_combined["CALCULATED TARIF"] = df_all_data_extracted_combined["DPP converted"][i]/df_all_data_extracted_combined["PAJAK PENGHASILAN converted"][i]*100
+                        df_all_data_extracted_combined["CALCULATED TARIF"] = df_all_data_extracted_combined["DPP"][i].replace(".","")/df_all_data_extracted_combined["PAJAK PENGHASILAN converted"][i]*100
 
                 # time.sleep(0.5)
 
@@ -217,6 +212,7 @@ if user_input_folder is not None:
 
 else :
     st.error("You have to upload pdf folder in the sidebar")
+
 
 
 
