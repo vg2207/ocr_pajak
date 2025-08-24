fa8815b0c1589b711d8cc2ca0d5a09a61b76d789
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-08-24 17:33:10 +0700

    Update ocr_pajak.py

--- ocr_pajak.py
+++ ocr_pajak.py
@@ -185,8 +185,10 @@ if user_input_folder is not None:
                     df_all_data_extracted = extract_text(image=img, coordinates=coordinates, all_data=df_all_data)
         
                     df_all_data_extracted_combined = pd.concat([df_all_data_extracted_combined, df_all_data_extracted]).reset_index(drop=True)
+
+                    time.sleep(0.5)
         
-                    st.dataframe(df_all_data_extracted_combined)
+                st.dataframe(df_all_data_extracted_combined)
 
 
         
@@ -285,6 +287,7 @@ else :
 
 # else :
 #     st.error("You have to upload a csv or an excel file in the sidebar")
+
 
 
 
