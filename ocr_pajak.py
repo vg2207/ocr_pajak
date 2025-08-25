8152e32839a837fea9a078ca7fe16d09cd538c0e
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2025-08-25 14:57:03 +0700

    Update ocr_pajak.py

--- ocr_pajak.py
+++ ocr_pajak.py
@@ -124,10 +124,11 @@ if user_input_folder is not None:
                     
                     # Open the PDF file
                     st.write(image_path_in_colab)
-                    current_filename=image_path_in_colab[(len(saved_directory)+1):][:-4]
+                    current_filename=image_path_in_colab[(len(saved_directory)+1):][:-4] + str(".pdf)
+                    
                     st.write(image_path_in_colab)
                     st.write(current_filename)
-                    reader = PdfReader(os.path.join(path_to_pdf,file_path_pdf[j]))
+                    reader = PdfReader(os.path.join(file_path_pdf, current_filename) )
                     a=[]
                     # Iterate through pages and extract text
                     extracted_text = ""
@@ -297,6 +298,7 @@ if user_input_folder is not None:
 
 else :
     st.error("You have to upload pdf folder in the sidebar")
+
 
 
 
